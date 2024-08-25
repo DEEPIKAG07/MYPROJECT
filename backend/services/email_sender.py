a6bc33fe8b45332ffa528f756cd5867976a6ddb9
DEEPIKA <deepika.gusai29@gmail.com>
2024-08-25 11:49:42 +0530

    Update email_sender.py

--- backend/services/email_sender.py
+++ backend/services/email_sender.py
@@ -1,1 +1,10 @@
+from flask_mail import Mail, Message
+from app import app
+
+mail = Mail(app)
+
+def send_email(subject, recipient, body):
+    msg = Message(subject, recipients=[recipient])
+    msg.body = body
+    mail.send(msg)
 
